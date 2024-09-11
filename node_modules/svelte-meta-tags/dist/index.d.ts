4b3366316e2d9e36cb7b0a80cc89ef7fd64c24fb
Dark Waves Tech <89140466+darkwaves-ofc@users.noreply.github.com>
2024-09-11 09:16:44 +0530

    Delete node_modules directory

--- node_modules/svelte-meta-tags/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as MetaTags } from './MetaTags.svelte';
-export { default as JsonLd } from './JsonLd.svelte';
-export type { MetaTagsProps, JsonLdProps, AdditionalRobotsProps, MobileAlternate, LanguageAlternate, Twitter, Facebook, OpenGraph, MetaTag, LinkTag } from './types';
--- node_modules/svelte-meta-tags/dist/index.js
+++
@@ -1,2 +0,0 @@
-export { default as MetaTags } from './MetaTags.svelte';
-export { default as JsonLd } from './JsonLd.svelte';
