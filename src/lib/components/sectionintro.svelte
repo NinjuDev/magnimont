ca420da3b1beb9231c87644601856258a782e314
Dark Waves <jgi.vimukthi@gmail.com>
2024-09-11 08:39:06 +0530

    Update sectionintro.svelte

--- src/lib/components/sectionintro.svelte
+++ src/lib/components/sectionintro.svelte
@@ -8,15 +8,16 @@
     data-aos-delay="50"
     data-aos-duration="1000"
     data-aos="flip-down"
-    class=" highlight font-calsans text-5xl pb-3 font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50"
+    class="highlight font-calsans text-3xl sm:text-4xl md:text-5xl lg:text-6xl pb-2 sm:pb-3 font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50"
   >
-    {name}<br />
+    {name}
+    <br />
   </h1>
   <p
     data-aos-delay="500"
     data-aos-duration="1000"
     data-aos="fade-in"
-    class="font-inter font-normal text-xs md:text-sm lg:text-base text-neutral-400 max-w-[60%] tracking-wider text-center mx-auto"
+    class="font-inter font-normal text-xs sm:text-sm md:text-base lg:text-lg text-neutral-400 max-w-[90%] sm:max-w-[80%] md:max-w-[70%] lg:max-w-[60%] tracking-wider text-center mx-auto"
   >
     {desc}
   </p>
